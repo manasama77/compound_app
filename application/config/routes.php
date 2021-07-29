c8b78ed132cee44035182491b45e8a713ef649f6
manasama77 <adam.pm77@gmail.com>
2021-07-29 13:42:05 +0700

    update login

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['default_controller']   = 'WelcomeController';
+$route['default_controller']   = 'LoginController/index';
 $route['404_override']         = '';
 $route['translate_uri_dashes'] = FALSE;
 
