e9af4d2f3effd3ecc06389bd7d08ec1ce17bba3d
manasama77 <adam.pm77@gmail.com>
2021-08-19 03:24:52 +0700

    update alert KYC

--- application/views/pages/member/profile/kyc.php
+++ application/views/pages/member/profile/kyc.php
@@ -98,7 +98,6 @@
 							</div>
 
 							<div class="alert alert-info">
-								<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 								<strong>Admin akan memproses KYC kamu maksimal 7 hari kerja (Senin ~ Jumat)</strong>
 							</div>
 
